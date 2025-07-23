8ceaaaf31037894525e172683ac0a6ff416d9dd8
LameroChubbyDuDu <proletarian.sad.dudu@gmail.com>
2025-07-24 07:44:05 +0800

    modified:   play/src/i18n/zh-CN/audio.ts

--- play/src/i18n/zh-CN/audio.ts
+++ play/src/i18n/zh-CN/audio.ts
@@ -3,11 +3,11 @@ import type { Translation } from "../i18n-types";
 
 const audio: DeepPartial<Translation["audio"]> = {
     manager: {
-        reduce: "说话时降低音乐音量",
-        allow: "播放声音",
+        reduce: "說話時降低音量",
+        allow: "播放聲音",
     },
-    message: "音频消息",
-    disable: "关掉你的麦克风",
+    message: "聲音設置",
+    disable: "關掉你的麥克風",
 };
 
 export default audio;
