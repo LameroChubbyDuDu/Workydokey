2fe3d4bd498ce6a546e71369916446e392756b7e
LameroChubbyDuDu <proletarian.sad.dudu@gmail.com>
2025-07-24 08:37:49 +0800

    modified:   play/src/i18n/zh-CN/error.ts

--- play/src/i18n/zh-CN/error.ts
+++ play/src/i18n/zh-CN/error.ts
@@ -3,25 +3,25 @@ import type { Translation } from "../i18n-types";
 
 const error: DeepPartial<Translation["error"]> = {
     accessLink: {
-        title: "访问链接错误",
-        subTitle: "找不到地图。请检查你的访问链接。",
-        details: "如果你想了解更多信息，你可以联系管理员或联系我们: hello@workadventu.re",
+        title: "訪問連結錯誤",
+        subTitle: "找不到地圖。請檢查你的訪問連結。",
+        details: "如果你想了解更多信息，你可以聯繫管理員或聯繫我們: hello@workadventu.re",
     },
     connectionRejected: {
-        title: "连接被拒绝",
-        subTitle: "你无法加入该世界。请稍后重试 {error}.",
-        details: "如果你想了解更多信息，你可以联系管理员或联系我们: hello@workadventu.re",
+        title: "連結被拒絕",
+        subTitle: "你無法加入該世界。請稍後重試 {error}.",
+        details: "如果你想了解更多信息，你可以聯繫管理員或連繫我們: hello@workadventu.re",
     },
     connectionRetry: {
-        unableConnect: "无法链接到 WorkAdventure. 请检查互联网连接。",
+        unableConnect: "無法連結到 WorkAdventure. 請檢查網路連接。",
     },
     errorDialog: {
-        title: "错误 😱",
-        hasReportIssuesUrl: "如果你想了解更多信息，你可以联系管理员或在以下网址报告问题:",
-        noReportIssuesUrl: "如果你想了解更多信息，你可以联系世界管理员。",
-        messageFAQ: "你也可以查看我们的:",
-        reload: "重新加载",
-        close: "关闭",
+        title: "錯誤 😱",
+        hasReportIssuesUrl: "如果你想了解更多信息，你可以聯繫管理員或在以下網址回報問題:",
+        noReportIssuesUrl: "如果你想了解更多信息，你可以聯繫世界管理員。",
+        messageFAQ: "你也可以查看我們的:",
+        reload: "重新載入",
+        close: "關閉",
     },
 };
 
